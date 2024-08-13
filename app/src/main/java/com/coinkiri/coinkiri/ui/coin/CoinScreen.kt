655df11304ed4d9d633ce57931c158ad8c3078bc
wjdrjs00 <kimjk3409@gmail.com>
2024-08-13 20:40:48 +0900

    refactor: 시세조회화면 리펙터링
    - 시세조회화면 content로 분리

--- app/src/main/java/com/coinkiri/coinkiri/ui/coin/CoinScreen.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/coin/CoinScreen.kt
@@ -1,100 +1,74 @@
 package com.coinkiri.coinkiri.ui.coin
 
-import android.graphics.fonts.Font
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.coinkiri.coinkiri.R
+import com.coinkiri.coinkiri.ui.coin.component.CoinItem
 import com.coinkiri.coinkiri.ui.coin.component.CoinScreenTopBar
-import com.coinkiri.coinkiri.ui.coin.component.SortingBar
-import com.coinkiri.coinkiri.ui.designsystem.theme.Black
+import com.coinkiri.coinkiri.ui.coin.component.CoinSortBar
 import com.coinkiri.coinkiri.ui.designsystem.theme.CoinkiriTheme
 import com.coinkiri.coinkiri.ui.designsystem.theme.White
-import java.text.DecimalFormat
 
 @Composable
 fun CoinScreen() {
     Scaffold(
         topBar = {
-            CoinScreenTopBar {
-
-            }
-
+            CoinScreenTopBar(
+                onBackIconClick = {},
+                onSearchIconClick = {}
+            )
         },
         content = { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxWidth()
-            ) {
-                SortingBar()
-                CoinItemList()
-            }
-
+            CoinScreenContent(innerPadding)
         }
     )
 }
 
-
 @Composable
-fun CoinItemList() {
-    LazyColumn() {
-        items(30) {
-            CoinItem()
-
-        }
-
-
+fun CoinScreenContent(
+    paddingValues: PaddingValues
+) {
+    Column(
+        modifier = Modifier
+            .padding(paddingValues)
+            .background(White)
+            .padding(top = 8.dp)
+            .fillMaxSize()
+    ) {
+        CoinSortBar(
+            onSortByCoinNameClick = {},
+            onSortByCurrentPriceClick = {},
+            onSortByChangeRateClick = {}
+        )
+        CoinItems()
     }
-
-
 }
 
-
 @Composable
+fun CoinItems() {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
     ) {
+        items(30) {
+            CoinItem(
+                onCoinItemClick = {}
             )
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CoinScreenPreview() {
     CoinkiriTheme {
         CoinScreen()
     }
-
 }
