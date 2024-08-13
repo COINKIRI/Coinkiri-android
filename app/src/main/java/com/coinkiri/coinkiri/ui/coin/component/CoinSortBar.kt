a827ab88dc9d004b4d72775e2dd6a87601b2dead
wjdrjs00 <kimjk3409@gmail.com>
2024-08-13 20:33:43 +0900

    refactor: 코인 시세조회 정렬바 리펙터링
    - SortButton 컴포저블을 생성하여 재사용 가능하도록 수정

---
+++ app/src/main/java/com/coinkiri/coinkiri/ui/coin/component/CoinSortBar.kt
@@ -0,0 +1,77 @@
+package com.coinkiri.coinkiri.ui.coin.component
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.coinkiri.coinkiri.ui.designsystem.theme.Black
+import com.coinkiri.coinkiri.ui.designsystem.theme.CoinkiriTheme
+
+@Composable
+fun CoinSortBar(
+    onSortByCoinNameClick: () -> Unit,
+    onSortByCurrentPriceClick: () -> Unit,
+    onSortByChangeRateClick: () -> Unit,
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp)
+    ) {
+        SortButton(
+            onClick = onSortByCoinNameClick,
+            btnText = "코인명 ⇅"
+        )
+        Spacer(Modifier.weight(1f))
+        SortButton(
+            onClick = onSortByCurrentPriceClick,
+            btnText = "현재가 ⇅"
+        )
+        SortButton(
+            onClick = onSortByChangeRateClick,
+            btnText = "변동률 ⇅"
+        )
+    }
+}
+
+@Composable
+fun SortButton(
+    onClick: () -> Unit,
+    btnText: String
+) {
+    TextButton(
+        onClick = onClick,
+        contentPadding = PaddingValues(3.dp),
+        modifier = Modifier
+            .height(25.dp)
+            .padding(horizontal = 3.dp)
+    ) {
+        Text(
+            text = btnText,
+            color = Black,
+            style = CoinkiriTheme.typography.labelSmall,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SortingBarPreview() {
+    CoinkiriTheme {
+        CoinSortBar(
+            onSortByCoinNameClick = {},
+            onSortByCurrentPriceClick = {},
+            onSortByChangeRateClick = {}
+        )
+    }
+}
