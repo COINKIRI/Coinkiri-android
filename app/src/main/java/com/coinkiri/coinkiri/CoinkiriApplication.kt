ab7dd9c1b59d712ff5b09052cf7c6aec19a5faec
wjdrjs00 <kimjk3409@gmail.com>
2024-08-18 14:16:54 +0900

    feat: kakaoSdk 초기화

--- app/src/main/java/com/coinkiri/coinkiri/CoinkiriApplication.kt
+++ app/src/main/java/com/coinkiri/coinkiri/CoinkiriApplication.kt
@@ -1,7 +1,18 @@
 package com.coinkiri.coinkiri
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class CoinkiriApplication: Application()
+class CoinkiriApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initKakaoSdk()
+    }
+
+    private fun initKakaoSdk() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
+}
