7d2d6b0fe0844f205bc1a1584d62bfadddd9ae10
wjdrjs00 <kimjk3409@gmail.com>
2024-08-24 01:02:11 +0900

    feat: Debug 상태일대만 log값 출력하도록 Timber 초기화 설정

--- app/src/main/java/com/coinkiri/coinkiri/CoinkiriApplication.kt
+++ app/src/main/java/com/coinkiri/coinkiri/CoinkiriApplication.kt
@@ -3,13 +3,19 @@ package com.coinkiri.coinkiri
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class CoinkiriApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        initTimber()
         initKakaoSdk()
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
     }
 
     private fun initKakaoSdk() {
