f3c2b9ec7b140ea131ffee0e914e7eafda69b589
wjdrjs00 <kimjk3409@gmail.com>
2024-08-24 01:18:03 +0900

    feat: REISSUE, JWT Qualifier 어노테이션 추가
    - 의존성 주입 시 특정 타입의 객체를 구분하기 위해 REISSUE, JWT Qualifier 어노테이션 정의

---
+++ app/src/main/java/com/coinkiri/coinkiri/core/network/Qualifier.kt
@@ -0,0 +1,11 @@
+package com.coinkiri.coinkiri.core.network
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class REISSUE
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class JWT
