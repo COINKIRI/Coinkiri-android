25ac71d898ada506a3f335796440ff6c62e0217b
wjdrjs00 <kimjk3409@gmail.com>
2024-08-27 00:22:48 +0900

    refactor: 기존 ChatScreen TalkDetailScreen으로 명명 변경

--- app/src/main/java/com/coinkiri/coinkiri/ui/talkdetail/TalkDetailScreen.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/talkdetail/TalkDetailScreen.kt
@@ -1,17 +1,14 @@
 package com.coinkiri.coinkiri.ui.talkdetail
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.coinkiri.coinkiri.core.designsystem.theme.CoinkiriTheme
 import com.coinkiri.coinkiri.ui.talkdetail.component.ChatContent
 import com.coinkiri.coinkiri.ui.talkdetail.component.ChatScreenTopBar
 
 @Composable
-fun ChatScreen() {
+fun TalkDetailScreen() {
     Scaffold(
         topBar = {
             ChatScreenTopBar()
@@ -22,10 +19,13 @@ fun ChatScreen() {
     )
 }
 
+
+
+
 @Preview
 @Composable
-fun ChatScreenPreview() {
+private fun TalkDetailScreenPreview() {
     CoinkiriTheme {
-        ChatScreen()
+        TalkDetailScreen()
     }
 }
