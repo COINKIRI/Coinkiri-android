4bd60d06e5ee032a54d8aa83905ecb094e3ce6a1
wjdrjs00 <kimjk3409@gmail.com>
2024-08-27 14:42:21 +0900

    refactor: TalkDetailContent -> Screen내부로 이동 및 layout 구조 수정

--- app/src/main/java/com/coinkiri/coinkiri/ui/talkdetail/TalkDetailScreen.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/talkdetail/TalkDetailScreen.kt
@@ -1,12 +1,23 @@
 package com.coinkiri.coinkiri.ui.talkdetail
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.coinkiri.coinkiri.core.designsystem.component.bottombar.BottomTextField
 import com.coinkiri.coinkiri.core.designsystem.component.topappbar.CoinkiriTopBar
 import com.coinkiri.coinkiri.core.designsystem.theme.CoinkiriTheme
-import com.coinkiri.coinkiri.ui.talkdetail.component.ChatContent
+import com.coinkiri.coinkiri.core.designsystem.theme.White
+import com.coinkiri.coinkiri.ui.talkdetail.component.SelectedCoinInfoItem
 
 @Composable
 fun TalkDetailScreen() {
@@ -16,8 +27,8 @@ fun TalkDetailScreen() {
                 onBackClick = {}
             )
         },
-        content = { paddingValues -> // paddingValues 적용
-            ChatContent(paddingValues)
+        content = { paddingValues ->
+            TalkDetailContent(paddingValues)
         },
         bottomBar = {
             BottomTextField()
@@ -36,7 +47,29 @@ private fun TalkDetailTopBar(
     )
 }
 
-
+@Composable
+private fun TalkDetailContent(
+    paddingValues: PaddingValues
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(White)
+            .padding(paddingValues)
+            .padding(horizontal = 8.dp)
+    ) {
+        SelectedCoinInfoItem(
+            onCoinInfoItemClick = {}
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            // TODO: talk 내용
+        }
+    }
+}
 
 @Preview
 @Composable
