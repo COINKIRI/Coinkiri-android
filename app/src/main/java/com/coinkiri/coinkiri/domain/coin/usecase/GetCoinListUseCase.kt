1096776bd8a91129bebec76c4f93c62fa79b8b64
wjdrjs00 <kimjk3409@gmail.com>
2024-08-29 20:15:42 +0900

    feat: 코인리스트를 가져오는 Usecase 정의

---
+++ app/src/main/java/com/coinkiri/coinkiri/domain/coin/usecase/GetCoinListUseCase.kt
@@ -0,0 +1,12 @@
+package com.coinkiri.coinkiri.domain.coin.usecase
+
+import com.coinkiri.coinkiri.domain.coin.entity.CoinResponseEntity
+import com.coinkiri.coinkiri.domain.coin.repository.CoinRepository
+import javax.inject.Inject
+
+class GetCoinListUseCase @Inject constructor(
+    private val coinRepository: CoinRepository
+) {
+    suspend operator fun invoke(): Result<List<CoinResponseEntity>> =
+        coinRepository.getCoinList()
+}
