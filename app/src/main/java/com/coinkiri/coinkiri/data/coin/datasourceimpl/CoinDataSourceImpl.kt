c4451221bb3142c0ae7235c6c09f5b5a6a8d84f9
wjdrjs00 <kimjk3409@gmail.com>
2024-08-29 20:10:38 +0900

    feat: CoinService로부터 데이터를 가져오기 위해 CoinDataSource 추가
    - 인터페이스 dataSource를 통해 메소드 명세
    - 구현체 dataSource에서 데이터 받음

---
+++ app/src/main/java/com/coinkiri/coinkiri/data/coin/datasourceimpl/CoinDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.coinkiri.coinkiri.data.coin.datasourceimpl
+
+import com.coinkiri.coinkiri.core.network.BaseResponse
+import com.coinkiri.coinkiri.data.coin.datasource.CoinDataSource
+import com.coinkiri.coinkiri.data.coin.dto.response.CoinResponseDto
+import com.coinkiri.coinkiri.data.coin.service.CoinService
+import javax.inject.Inject
+
+class CoinDataSourceImpl @Inject constructor(
+    private val coinService: CoinService
+) : CoinDataSource {
+
+    override suspend fun getCoinList(): BaseResponse<List<CoinResponseDto>> =
+        coinService.getCoinList()
+}
