f0139d5ce1b644c8b14dccbc82d4f018483f1712
wjdrjs00 <kimjk3409@gmail.com>
2024-09-03 07:55:27 +0900

    feat: 요청,응답 데이터 가공을 위해 TickerRepository 추가
    - 인터페이스 Repository에서 메소드 명세
    - 구현체 Repository
        - request Entity ->  DTO  데이터 변환
        - response DTO ->  Entity  데이터 변환

---
+++ app/src/main/java/com/coinkiri/coinkiri/data/ticker/repositoryimpl/TickerRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.coinkiri.coinkiri.data.ticker.repositoryimpl
+
+import com.coinkiri.coinkiri.data.ticker.datasource.TickerDataSource
+import com.coinkiri.coinkiri.data.ticker.mapper.toTickerEntity
+import com.coinkiri.coinkiri.data.ticker.mapper.toTickerRequestDto
+import com.coinkiri.coinkiri.domain.coin.entity.request.TickerRequestEntity
+import com.coinkiri.coinkiri.domain.coin.entity.response.TickerResponseEntity
+import com.coinkiri.coinkiri.domain.coin.repository.TickerRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class TickerRepositoryImpl @Inject constructor(
+    private val tickerDataSource: TickerDataSource
+) : TickerRepository {
+
+    override fun getTickers(tickerRequestEntity: TickerRequestEntity): Flow<TickerResponseEntity> =
+        tickerDataSource.getTickers(tickerRequestEntity.toTickerRequestDto())
+            .map { tickerResponseDto ->
+                tickerResponseDto.toTickerEntity()
+            }
+
+    override fun closeConnection() {
+        tickerDataSource.closeConnection()
+    }
+}
