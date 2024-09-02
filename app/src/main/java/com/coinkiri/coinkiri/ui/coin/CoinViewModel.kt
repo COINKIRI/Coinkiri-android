bdcd40db268ee8612ea5e427ebf5032610c5812a
wjdrjs00 <kimjk3409@gmail.com>
2024-09-03 08:07:16 +0900

    feat: Ticker데이터를 받아 기존 리스트 데이터에 추가하도록 구현
    - fetchTickers(): marketNames을 통해 Ticker정보를 요청
    - updateTickers(): 새롭게 받은 Ticker값으 기존 라스트에 추가

--- app/src/main/java/com/coinkiri/coinkiri/ui/coin/CoinViewModel.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/coin/CoinViewModel.kt
@@ -2,9 +2,11 @@ package com.coinkiri.coinkiri.ui.coin
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.coinkiri.coinkiri.domain.coin.entity.response.CoinResponseEntity
 import com.coinkiri.coinkiri.domain.coin.entity.MergedCoinTickerEntity
+import com.coinkiri.coinkiri.domain.coin.entity.request.TickerRequestEntity
+import com.coinkiri.coinkiri.domain.coin.entity.response.TickerResponseEntity
 import com.coinkiri.coinkiri.domain.coin.usecase.GetCoinListUseCase
+import com.coinkiri.coinkiri.domain.coin.usecase.GetTicketsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -14,6 +16,7 @@ import javax.inject.Inject
 @HiltViewModel
 class CoinViewModel @Inject constructor(
     private val getCoinListUseCase: GetCoinListUseCase,
+    private val getTicketsUseCase: GetTicketsUseCase,
 ) : ViewModel() {
 
     private val _mergedCoinTickerList = MutableStateFlow<List<MergedCoinTickerEntity>>(emptyList())
@@ -36,10 +39,40 @@ class CoinViewModel @Inject constructor(
                         )
                     }
                     _mergedCoinTickerList.value = initialMergedList
+                    fetchTickers()
                 }
                 .onFailure { exception ->
                     "${exception.message}"
                 }
         }
+    }
+
+    private fun fetchTickers() {
+        val marketNames = _mergedCoinTickerList.value.joinToString(",") { "\"${it.marketName}\"" }
+        viewModelScope.launch {
+            getTicketsUseCase(TickerRequestEntity(TYPE, marketNames))
+                .collect { tickerResponseEntity ->
+                    updateTickers(listOf(tickerResponseEntity))
+                }
+        }
+    }
+
+    private fun updateTickers(tickers: List<TickerResponseEntity>) {
+        val updatedList = _mergedCoinTickerList.value.map { mergedCoinTicker ->
+            val matchingTicker = tickers.find { it.code == mergedCoinTicker.marketName }
+            if (matchingTicker != null) {
+                mergedCoinTicker.copy(
+                    tradePrice = matchingTicker.tradePrice,
+                    signedChangeRate = matchingTicker.signedChangeRate
+                )
+            } else {
+                mergedCoinTicker
+            }
+        }
+        _mergedCoinTickerList.value = updatedList
+    }
+
+    companion object {
+        private const val TYPE = "ticker"
     }
 }
