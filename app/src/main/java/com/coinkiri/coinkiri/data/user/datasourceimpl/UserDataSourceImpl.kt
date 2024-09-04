852f71f5a6bea1c912129ee260664c1c469b2dce
박지민 <pjm5489@naver.com>
2024-09-05 00:15:08 +0900

    feat: UserDataSource 추가
    UserService를 통해 데이터를 가져옴
    -UserDataSource를 통해 메서드 명세
    -UserDataSource구현체

---
+++ app/src/main/java/com/coinkiri/coinkiri/data/user/datasourceimpl/UserDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.coinkiri.coinkiri.data.user.datasourceimpl
+
+import com.coinkiri.coinkiri.core.network.BaseResponse
+import com.coinkiri.coinkiri.data.user.datasource.UserDataSource
+import com.coinkiri.coinkiri.data.user.dto.UserResponseDto
+import com.coinkiri.coinkiri.data.user.service.UserService
+import jakarta.inject.Inject
+
+class UserDataSourceImpl @Inject constructor(
+    private val userService: UserService
+) : UserDataSource {
+
+    override suspend fun getUser(accessToken: String): BaseResponse<UserResponseDto> =
+        userService.getUser(accessToken)
+}
