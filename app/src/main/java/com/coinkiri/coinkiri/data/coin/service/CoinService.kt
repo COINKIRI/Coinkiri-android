e5506d74a84bae43c3ce378b6df01652d5117223
wjdrjs00 <kimjk3409@gmail.com>
2024-09-05 19:57:29 +0900

    feat: CoinService 상세정보 수신을 위해 getCoinDetailInfo메서드 추가

--- app/src/main/java/com/coinkiri/coinkiri/data/coin/service/CoinService.kt
+++ app/src/main/java/com/coinkiri/coinkiri/data/coin/service/CoinService.kt
@@ -1,10 +1,17 @@
 package com.coinkiri.coinkiri.data.coin.service
 
 import com.coinkiri.coinkiri.core.network.BaseResponse
+import com.coinkiri.coinkiri.data.coin.dto.response.CoinDetailResponseDto
 import com.coinkiri.coinkiri.data.coin.dto.response.CoinResponseDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CoinService {
     @GET("/api/v1/coin/all")
     suspend fun getCoinList(): BaseResponse<List<CoinResponseDto>>
+
+    @GET("/api/v1/coin/detail")
+    suspend fun getCoinDetailInfo(
+        @Query("market") market: String
+    ): BaseResponse<CoinDetailResponseDto>
 }
