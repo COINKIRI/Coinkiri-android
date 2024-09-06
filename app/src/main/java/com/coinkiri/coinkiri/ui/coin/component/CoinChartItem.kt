66842c2efa9d82d9d7d3ea8b3707cf115045820c
wjdrjs00 <kimjk3409@gmail.com>
2024-09-06 20:49:28 +0900

    feat: mpchart 활용하여 시세차트 구현(임시)

--- app/src/main/java/com/coinkiri/coinkiri/ui/coin/component/CoinChartItem.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/coin/component/CoinChartItem.kt
@@ -1,26 +1,165 @@
 package com.coinkiri.coinkiri.ui.coin.component
 
+import android.graphics.drawable.GradientDrawable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.coinkiri.coinkiri.R
 import com.coinkiri.coinkiri.core.designsystem.theme.CoinkiriTheme
+import com.coinkiri.coinkiri.core.designsystem.theme.Gray400
+import com.coinkiri.coinkiri.core.designsystem.theme.Red
+import com.coinkiri.coinkiri.ui.coin.model.CoinDetailModel
+import com.coinkiri.coinkiri.ui.coin.model.PriceModel
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 @Composable
-fun CoinChartItem() {
+fun CoinChartItem(
+    coinDetailInfo: CoinDetailModel
+) {
+    val reversedCoinInfo = coinDetailInfo.price.reversed()
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .padding(vertical = 15.dp)
     ) {
-        Text(text = "여기 차트")
+        ChartInfo()
+        ChartViewItem(reversedCoinInfo)
     }
 }
 
-@Preview
+@Composable
+private fun ChartInfo() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                vertical = 5.dp,
+                horizontal = 10.dp
+            )
+    ) {
+        Text(
+            text = "시세차트",
+            style = CoinkiriTheme.typography.titleLarge
+        )
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(3.dp),
+            modifier = Modifier.padding(start = 3.dp)
+        ) {
+            Text(
+                text = "0000년 00월 00일, 09시 00분",
+                fontWeight = FontWeight.SemiBold,
+                style = CoinkiriTheme.typography.titleMedium,
+                color = Gray400
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_circle_up),
+                    contentDescription = "",
+                    tint = Color.Green,
+                )
+                Text(
+                    text = "₩ 100,000,000",
+                    fontWeight = FontWeight.SemiBold,
+                    style = CoinkiriTheme.typography.titleMedium,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun ChartViewItem(
+    reversedCoinInfo: List<PriceModel>
+) {
+    val maxValue = reversedCoinInfo.maxOfOrNull { it.tradePrice.toFloat() } ?: 0f
+    val minValue = reversedCoinInfo.minOfOrNull { it.tradePrice.toFloat() } ?: 0f
+
+    AndroidView(
+        factory = { context ->
+            val chart = LineChart(context)
+
+            // chart 기본 세팅
+            chart.description.isEnabled = false
+            chart.legend.isEnabled = false
+            chart.setPinchZoom(true)
+            chart.setTouchEnabled(true)
+
+            // chart 축 설정
+            chart.xAxis.isEnabled = false
+            chart.xAxis.setDrawGridLines(false)
+            chart.axisLeft.isEnabled = false
+            chart.axisLeft.setDrawGridLines(false)
+            chart.axisRight.axisMaximum = maxValue
+            chart.axisRight.axisMinimum = minValue
+            chart.axisRight.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+
+            chart
+        },
+        update = { chart ->
+            val entries = reversedCoinInfo
+                .mapIndexed { index, reversedCoinInfo ->
+                    Entry(
+                        index.toFloat(),
+                        reversedCoinInfo.tradePrice.toFloat()
+                    )
+                }
+
+            val startColor = Red.copy(alpha = 0.3f).toArgb()
+            val endColor = Red.copy(alpha = 0f).toArgb()
+
+            val gradientDrawable = GradientDrawable(
+                GradientDrawable.Orientation.TOP_BOTTOM,
+                intArrayOf(startColor, endColor)
+            )
+
+            val dataSet = LineDataSet(entries, "coinDetail")
+                .apply {
+                    fillDrawable = gradientDrawable
+                    mode = LineDataSet.Mode.LINEAR
+                    color = Red.toArgb()
+                    lineWidth = 1f
+                    setDrawCircles(false)
+                    setDrawValues(false)
+                    setDrawFilled(true)
+                }
+
+            chart.data = LineData(dataSet)
+            chart.invalidate()
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@Preview(showBackground = true)
 @Composable
 private fun CoinChartItemPreview() {
     CoinkiriTheme {
-        CoinChartItem()
+        CoinChartItem(
+            coinDetailInfo = CoinDetailModel()
+        )
     }
 }
