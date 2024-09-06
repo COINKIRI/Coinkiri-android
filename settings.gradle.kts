bd5a755e1bc4fce0e02891225891f8a32c7e1cca
wjdrjs00 <kimjk3409@gmail.com>
2024-09-06 15:09:55 +0900

    chore: maven { url } 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,10 +16,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/")
-            url = java.net.URI("https://jitpack.io")
-        }
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven { url = java.net.URI("https://jitpack.io") }
     }
 }
 
