052daaf2b521a4e4581a86d1918b4cd391c0cf67
wjdrjs00 <kimjk3409@gmail.com>
2024-09-06 10:08:03 +0900

    chore: mpAndroidChart 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -114,6 +114,8 @@ dependencies {
     implementation(libs.hilt.navigation.compose)
     ksp(libs.hilt.compiler)
 
+    implementation(libs.mpAndroidChart)
+
     implementation("io.coil-kt:coil-compose:2.6.0")
 
     testImplementation(libs.junit)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven {
+            url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/")
+            url = java.net.URI("https://jitpack.io")
+        }
     }
 }
 
