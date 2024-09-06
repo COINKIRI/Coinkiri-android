d0fe94912c53805131876929305dd1cbd4acf707
wjdrjs00 <kimjk3409@gmail.com>
2024-09-06 20:36:20 +0900

    fix: GetTickersUseCase 오타 수정

--- app/src/main/java/com/coinkiri/coinkiri/domain/ticker/usecase/GetTickersUseCase.kt
+++ app/src/main/java/com/coinkiri/coinkiri/domain/ticker/usecase/GetTickersUseCase.kt
@@ -6,7 +6,7 @@ import com.coinkiri.coinkiri.domain.ticker.repository.TickerRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetTicketsUseCase @Inject constructor(
+class GetTickersUseCase @Inject constructor(
     private val tickerRepository: TickerRepository
 ) {
     operator fun invoke(tickerRequestEntity: TickerRequestEntity): Flow<TickerResponseEntity> =
