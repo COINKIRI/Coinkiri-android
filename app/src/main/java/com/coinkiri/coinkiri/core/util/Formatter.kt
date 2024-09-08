e4e0dbcce93d08eb60ff159d18dba12684110c7e
wjdrjs00 <kimjk3409@gmail.com>
2024-09-08 18:05:49 +0900

    feat: 포멧터 formatDateTime메서드 추가 및 상수화

--- app/src/main/java/com/coinkiri/coinkiri/core/util/Formatter.kt
+++ app/src/main/java/com/coinkiri/coinkiri/core/util/Formatter.kt
@@ -1,16 +1,46 @@
 package com.coinkiri.coinkiri.core.util
 
 import java.text.DecimalFormat
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 object Formatter {
 
+    private const val PRICE_FORMAT = "#,###"
+    private const val DEFAULT_PRICE = "0"
+
+    private const val SIGNED_CHANGE_RATE_FORMAT = "%.2f"
+    private const val PERCENT_SYMBOL = "%"
+
+    private const val KRW_PREFIX = "KRW-"
+    private const val EMPTY_STRING = ""
+
+    private const val DATE_INPUT_FORMAT = "yyyy-MM-dd'T'HH:mm:ss"
+    private const val DATE_OUTPUT_FORMAT = "yyyy년 MM월 dd일, HH시 mm분"
+
     fun formattedPrice(price: Double?): String =
-        price?.let { DecimalFormat("#,###").format(it) } ?: "0"
+        price?.let { DecimalFormat(PRICE_FORMAT).format(it) } ?: DEFAULT_PRICE
 
     fun formattedSignedChangeRate(signedChangeRate: Double?): String =
-        String.format(Locale.KOREAN, "%.2f", signedChangeRate?.times(100) ?: 0.0) + "%"
+        String.format(
+            Locale.KOREAN,
+            SIGNED_CHANGE_RATE_FORMAT,
+            signedChangeRate?.times(100) ?: 0.0
+        ) + PERCENT_SYMBOL
 
     fun removeKrWPrefix(input: String?): String =
-        input?.replace("KRW-", "") ?: ""
+        input?.replace(KRW_PREFIX, EMPTY_STRING) ?: EMPTY_STRING
+
+    fun formatDateTime(input: String?): String {
+        return input?.let {
+            try {
+                val formatter = DateTimeFormatter.ofPattern(DATE_INPUT_FORMAT)
+                val dateTime = java.time.LocalDateTime.parse(it, formatter)
+                val outputFormatter = DateTimeFormatter.ofPattern(DATE_OUTPUT_FORMAT)
+                dateTime.format(outputFormatter)
+            } catch (e: Exception) {
+                EMPTY_STRING
+            }
+        } ?: EMPTY_STRING
+    }
 }
