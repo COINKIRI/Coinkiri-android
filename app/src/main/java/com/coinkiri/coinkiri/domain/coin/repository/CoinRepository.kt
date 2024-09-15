cc0c3218ee39419ce90b6609a11857d40c3cb6ea
wjdrjs00 <kimjk3409@gmail.com>
2024-09-15 17:05:03 +0900

    feat: CoinRespository -> getCoinRiseAndFallCount() 메서드 추가

--- app/src/main/java/com/coinkiri/coinkiri/domain/coin/repository/CoinRepository.kt
+++ app/src/main/java/com/coinkiri/coinkiri/domain/coin/repository/CoinRepository.kt
@@ -1,5 +1,6 @@
 package com.coinkiri.coinkiri.domain.coin.repository
 
+import com.coinkiri.coinkiri.domain.coin.entity.response.CoinCountResponseEntity
 import com.coinkiri.coinkiri.domain.coin.entity.response.CoinDetailResponseEntity
 import com.coinkiri.coinkiri.domain.coin.entity.response.CoinResponseEntity
 
@@ -8,5 +9,7 @@ interface CoinRepository {
     suspend fun getCoinList(): Result<List<CoinResponseEntity>>
 
     suspend fun getCoinDetail(market: String): Result<CoinDetailResponseEntity>
+
+    suspend fun getCoinRiseAndFallCount(): Result<CoinCountResponseEntity>
 
 }
