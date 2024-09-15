8c1539d2d404c897382c8681bd4342936aea318e
wjdrjs00 <kimjk3409@gmail.com>
2024-09-15 17:14:48 +0900

    feat: 유저 정보값 요청을 위한 fetchUserInfo() 메소드 추가

--- app/src/main/java/com/coinkiri/coinkiri/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/coinkiri/coinkiri/ui/home/HomeViewModel.kt
@@ -3,7 +3,10 @@ package com.coinkiri.coinkiri.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.coinkiri.coinkiri.domain.coin.usecase.GetCoinRiseAndFallCountUseCase
+import com.coinkiri.coinkiri.domain.user.entity.UserResponseEntity
+import com.coinkiri.coinkiri.domain.user.usecase.GetUserInfoUseCase
 import com.coinkiri.coinkiri.ui.home.model.CoinCountModel
+import com.coinkiri.coinkiri.ui.profile.model.UserInfoModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,12 +15,17 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val getCoinRiseAndFallCountUseCase: GetCoinRiseAndFallCountUseCase
+    private val getCoinRiseAndFallCountUseCase: GetCoinRiseAndFallCountUseCase,
+    private val getUserInfoUseCase: GetUserInfoUseCase
 ) : ViewModel() {
 
     private val _coinRiseAndFallCount = MutableStateFlow(CoinCountModel())
     val coinRiseAndFallCount: StateFlow<CoinCountModel>
         get() = _coinRiseAndFallCount
+
+    private val _userInfo = MutableStateFlow(UserInfoModel())
+    val userInfo: StateFlow<UserInfoModel>
+        get() = _userInfo
 
     suspend fun fetchCoinRiseAndFallCount() {
         viewModelScope.launch {
@@ -34,5 +42,25 @@ class HomeViewModel @Inject constructor(
                     "${exception.message}"
                 }
         }
+    }
+
+    suspend fun fetchUserInfo() =
+        viewModelScope.launch {
+            val result = getUserInfoUseCase()
+
+            result
+                .onSuccess { userResponseEntity ->
+                    handleFetchUserInfoSuccess(userResponseEntity)
+                }
+                .onFailure { exception ->
+                    "${exception.message}"
+                }
+        }
+
+    private fun handleFetchUserInfoSuccess(userResponseEntity: UserResponseEntity) {
+        _userInfo.value = UserInfoModel(
+            nickname = userResponseEntity.nickname,
+            pic = userResponseEntity.pic
+        )
     }
 }
